cd1fd2a4bf9fbf8d69fed4593eaab15e9ed5a60a
DaisyChepyegon <daisychepyegon@gmail.com>
2022-05-15 17:28:37 +0300

    add functions

--- index.js
+++ index.js
@@ -9,6 +9,45 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions,correctQuestionsIndex;
 let quizScore=0;
 
+
+
+function startGame(){
+  startButton.classList.add('hide')
+  shuffledQuestions=questions.sort(()=>Math.random( -0.5))
+  currentQuestionIndex=0;
+  questionContainerElement.classList.remove("hide")
+  setnextQuestion()
+  quizScore=0
+}
+
+function setnextQuestion(){
+  resetState();
+  showquestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showquestion(question) { 
+  questionElement.innerText=question.question;
+  question.answers.forEach((answer)=>{
+    const button=document.createElement('button')
+    button.innerText=answer.text;
+    button.classList.add('btn')
+    if(answer.correct){
+      button.dataset.correct=answer.correct
+    }
+    button.addEventListener('click',selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState(){
+  clearStatusClass(document.body)
+  nextButtons.classList.add('hide')
+  while(answerButtonsElement.firstChild){
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+
 function selectAnswer(e){
   const selectedButtons=e.target
   const correct=selectedButton.dataset.correct
@@ -17,7 +56,16 @@ function selectAnswer(e){
   Array.from(answerButtonElement.children).forEach((button)=>{
     setStatusClass(button,button.dataset.correct)
   })
-  
+  if(shuffledQuestions.length>currentQuestionIndex +1){
+    nextButton.classList.remove("hide")
+  }else{
+    startButton.innerText="restart"
+    startButton.classList.remove("hide")
+  }
+  if(selectedButtons.dataset=correct){
+    quizScore++
+  }
+  document.getElementById('right-answer').innerText=quizScore
 }
 
 
